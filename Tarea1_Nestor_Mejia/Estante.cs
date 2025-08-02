7607984e9d08b9bf9793e3085a1fdfacd2c3ce3e
nmejia1902 <159928835+nmejia1902@users.noreply.github.com>
2025-08-02 15:10:31 -0600

    1ra Publicacion

---
+++ Tarea1_Nestor_Mejia/Estante.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Estante
+{
+    public int NumeroEstante { get; set; }
+    public int CapacidadMaxima { get; set; }
+    public List<Libro> LibrosEnEstante { get; set; }
+
+    public Estante(int numeroEstante, int capacidadMaxima)
+    {
+        NumeroEstante = numeroEstante;
+        CapacidadMaxima = capacidadMaxima;
+        LibrosEnEstante = new List<Libro>();
+    }
+
+    public void MostrarContenido()
+    {
+        Console.WriteLine($"\nEstante N° {NumeroEstante} (Capacidad Máxima: {CapacidadMaxima} libros)");
+        Console.WriteLine("Libros en el estante:");
+        foreach (var libro in LibrosEnEstante)
+        {
+            libro.MostrarInformacion();
+        }
+    }
+}
---
+++ Tarea1_Nestor_Mejia/Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        Libro libro1 = new Libro("El Principito", "Antoine de Saint-Exupéry", 1943);
+        Libro libro2 = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967);
+        Libro libro3 = new Libro("1984", "George Orwell", 1949);
+
+
+        Persona persona1 = new Persona("Nestor", "Mejia", 38);
+        Persona persona2 = new Persona("Carlos", "López", 30);
+
+ 
+        Estante estante = new Estante(1, 5);
+        estante.LibrosEnEstante.Add(libro1);
+        estante.LibrosEnEstante.Add(libro2);
+        estante.LibrosEnEstante.Add(libro3);
+
+        Console.WriteLine("Información de los libros:");
+        libro1.MostrarInformacion();
+        libro2.MostrarInformacion();
+        libro3.MostrarInformacion();
+
+        Console.WriteLine("\nSaludos de las personas:");
+        persona1.Saludar();
+        persona2.Saludar();
+
+        estante.MostrarContenido();
+
+        Console.WriteLine("\nPresiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
